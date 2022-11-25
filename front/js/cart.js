1c20cc1ddbc672b6c0dd80252d30b362bb607a25
Bulrack66 <faguowinien@gmail.com>
2022-11-25 21:41:21 +0100

    cart product ok

--- front/js/cart.js
+++ front/js/cart.js
@@ -3,15 +3,11 @@ const displayCart  = document.getElementById('cart__items');
 let cart           = JSON.parse(localStorage.getItem("tab"));
 let totalQuantity  = document.getElementById('totalQuantity');
 let totalPriceCart = document.getElementById('totalPrice');
-
 let totalArticle = () => {
-
     let total = 0
-
     cart.forEach(a => {
         total += a.quantity
     });
-
     totalQuantity.innerHTML = total;
 }
 
@@ -40,7 +36,6 @@ const displaySofa = (sofaData) => {
         })
         return mySofa
     })
-    
     mySofa.forEach(s =>{    
         displayCart.innerHTML += `<article class="cart__item" data-id="${s.id}" data-color="${s.color}">
         <div class="cart__item__img"><img src="${s.image}" alt="${s.alt}"></div>
@@ -49,28 +44,27 @@ const displaySofa = (sofaData) => {
         <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${s.quantity}">
         </div><div class="cart__item__content__settings__delete"><p class="deleteItem">Supprimer</p></div></div></div></article>`
     });
-    let totalSofaCartPrice = 0
-
+    let totalSofaCartPrice = 0;
     mySofa.forEach(tp =>{
-        totalSofaCartPrice += tp.totalPrice
+        totalSofaCartPrice += tp.totalPrice;
     });
-
     totalPriceCart.innerHTML = totalSofaCartPrice;
     totalArticle();
-    
-    let getQuantities    = document.querySelectorAll('.itemQuantity');
+    let getQuantities = document.querySelectorAll('.itemQuantity');
+    let deleteProducts = document.getElementsByClassName('deleteItem')
     let newCart = [];
     for (const getQuantity of getQuantities) {
-        getQuantity.addEventListener('input',(e) =>{
+        getQuantity.addEventListener('input',(e) => {
             let newItems = {
             id : e.target.parentElement.parentElement.parentElement.parentElement.getAttribute('data-id'),
             color : e.target.parentElement.parentElement.parentElement.parentElement.getAttribute('data-color'),
             quantity : Number(e.target.value)
             }
-            newCart.push(newItems)
-            let isNew = true
+            console.log(e),
+            newCart.push(newItems);
+            let isNew = true;
             cart = cart.map(t =>{
-                if (t.id === newItems.id && t.color === newItems.color && newItems.quantity!= 0) {
+                if (t.id === newItems.id && t.color === newItems.color && newItems.quantity!= 0 && newItems.quantity <= 100) {
                     t.quantity = newItems.quantity;
                     isNew = false
                 }
@@ -82,15 +76,24 @@ const displaySofa = (sofaData) => {
                 }
                 return t
             });
-            
             window.location.reload()
             window.localStorage.setItem("tab", JSON.stringify(cart));
-
+            
         })
     }
-
+    for (const deleteProduct of deleteProducts) {
+        deleteProduct.addEventListener('click', (e) => {
+            let deleteMySofa = e.path[4].dataset;
+            cart = cart.filter(cc => {
+                if(cc.id === deleteMySofa.id && cc.color === deleteMySofa.color){
+                    return false
+                }else return true
+                })
+            window.location.reload()
+            window.localStorage.setItem("tab", JSON.stringify(cart));
+            console.log(deleteMySofa.id, deleteMySofa.color);
+        })
+    }
 }
-
-    
 console.log(document.forms);
 
